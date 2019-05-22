eb4f2301add7f09433e9becb60d90f587230be0d
Simon Heid <simon_heid@t-online.de>
2019-05-22 22:22:42 +0200

    create solution and projects
    close #1

---
+++ src/MFE.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MFE.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/MFE/IMusicVolumeEvener.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MFE
+{
+    interface IMusicVolumeEvener
+    {
+        // TODO add method(s) here
+    }
+}
