0f75b2ccc096ddbe81d534d16b3411e405624697
heilmannha <49225043+heilmannha@users.noreply.github.com>
2019-06-04 12:28:29 +0200

    Added implementation of AudioFile, WaveSound & IOAssistant

---
+++ src/MFE/AudioFile.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+  internal abstract class AudioFile<IdentifierType>
+  {
+    /// <summary>
+    /// Defines a formula that returns the relative weight of a sample regarding average calculation.
+    /// </summary>
+    /// <param name="ratio">absolute ratio |sample / average|</param>
+    /// <returns>the relative weight of the sample</returns>
+    public delegate double WeighSampleFormula(double ratio);
+
+    /// <summary>
+    /// Defines a formula that returns the adjusted sample.
+    /// </summary>
+    /// <param name="sample">the sample to be adjusted [-1,1]</param>
+    /// <param name="targetAverage">the weighted average to reach [0,1]</param>
+    /// <param name="currentAverage">the weighted average of the current file [0,1]</param>
+    /// <returns>the new sample [-1,1]</returns>
+    public delegate double AdjustSampleFormula(double sample, double targetAverage, double currentAverage);
+
+    /// <summary>
+    /// Defines a formula that returns the relative weight of a sample regarding average calculation. The default is 1 for all ratios.
+    /// </summary>
+    public static WeighSampleFormula WeightFormula {
+      get => wsf;
+      set => wsf = value ?? wsf;
+    }
+
+    /// <summary>
+    /// Defines a formula that returns the adjusted sample. The default is sample * targetAverage / currentAverage.
+    /// </summary>
+    public static AdjustSampleFormula AdjustmentFormula {
+      get => asf;
+      set => asf = value ?? asf;
+    }
+
+    private static WeighSampleFormula wsf = ratio => 1d;
+    private static AdjustSampleFormula asf = (sample, target, current) => sample * target / current;
+
+    /// <summary>
+    /// Creates an AudioFile from a buffer.
+    /// </summary>
+    /// <param name="buffer">the file to modify</param>
+    /// <param name="token"></param>
+    /// <returns>the created AudioFile on success, null otherwise</returns>
+    public static async Task<AudioFile<IdentifierType>> FromBuffer(byte[] buffer, CancellationToken token = default(CancellationToken))
+    {
+      return await Task.Run(() => {
+        try {
+          return new WaveSound<IdentifierType>(buffer);
+        }
+        catch {
+          return null;
+        }
+      }, token);
+    }
+
+    /// <summary>
+    /// optional - an object that can be used to identify the AudioFile
+    /// </summary>
+    public IdentifierType Identifier { get; set; }
+
+    /// <summary>
+    /// the weighted average as calculated using AudioFile.WeightFormula
+    /// </summary>
+    public double WeightedAverage { get; }
+
+    /// <summary>
+    /// the highest amplitude of all samples [0,1]
+    /// </summary>
+    public double Highest { get; }
+
+    protected AudioFile(byte[] buffer)
+    {
+      Identifier = default(IdentifierType);
+      double[] samples = DecodeSamples(buffer);
+      Highest = samples.Max(sample => Math.Abs(sample));
+      double average = samples.Average(sample => Math.Abs(sample)), dividend = 0d, divisor = 0d;
+      foreach (double sample in samples) {
+        double weight = WeightFormula(sample / average);
+        dividend += sample * weight;
+        divisor += weight;
+      }
+      WeightedAverage = dividend / divisor;
+    }
+
+    /// <summary>
+    /// The samples are read from the buffer, adjusted and written back to it.
+    /// </summary>
+    /// <param name="targetAverage">the average to reach</param>
+    /// <param name="buffer">the buffer that contains the file to be modified - This will contain the new bytes on success.</param>
+    /// <param name="token"></param>
+    /// <returns>true on success, false on error</returns>
+    public async Task<bool> AdjustSamples(double targetAverage, byte[] buffer, CancellationToken token = default(CancellationToken))
+    {
+      return await Task.Run(() => {
+        if (WouldFit(targetAverage)) {
+          try {
+            EncodeSamples(DecodeSamples(buffer).Select(sample => AdjustmentFormula(sample, targetAverage, WeightedAverage)), buffer).CopyTo(buffer, 0L);
+            return true;
+          }
+          catch { }
+        }
+        return false;
+      }, token);
+    }
+
+    /// <summary>
+    /// Checks whether reaching this target average does not cause overmodulation.
+    /// </summary>
+    /// <param name="targetAverage">the average to reach</param>
+    /// <returns>true if the samples fit, false on overmodulation</returns>
+    public bool WouldFit(double targetAverage) => 1d >= AdjustmentFormula(Highest, targetAverage, WeightedAverage);
+
+    protected abstract double[] DecodeSamples(byte[] buffer);
+    protected abstract byte[] EncodeSamples(IEnumerable<double> samples, byte[] buffer);
+  }
+}
---
+++ src/MFE/WaveSound.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestConsole
+{
+  internal class WaveSound<T> : AudioFile<T>
+  {
+    public WaveSound(byte[] buffer) : base(buffer) { }
+
+    private bool _little_endian;
+    private int step;
+    private long dataChunkStart;
+
+    protected override double[] DecodeSamples(byte[] buffer)
+    {
+      _little_endian = buffer[3] == 0x46;
+      if ((Read(buffer, 0, 4, false) == 0x52494658 | Read(buffer, 0, 4, false) == 0x52494646) & Read(buffer, 8, 4, false) == 0x57415645 & Read(buffer, 12, 4, false) == 0x666d7420 & Read(buffer, 20, 2) == 1) {
+        dataChunkStart = 28 + Read(buffer, 16, 4);
+        long dataChunkEnd = Read(buffer, dataChunkStart - 4, 4) + dataChunkStart;
+        step = (Read(buffer, 34, 2) + 7) / 8;
+        double[] samples = new double[(dataChunkEnd - dataChunkStart) / step];
+        for (long i = dataChunkStart, n = 0; i < dataChunkEnd; i += step)
+          samples[n++] = (Read(buffer, i, step) << ((4 - step) * 8)) / (double)int.MaxValue;
+        return samples;
+      }
+      else
+        throw new ArgumentException("The buffer does not contain a valid RIFF/RIFX wavesound in PCM format.", "buffer");
+    }
+
+    protected override byte[] EncodeSamples(IEnumerable<double> samples, byte[] buffer)
+    {
+      long i = dataChunkStart;
+      byte[] output = new byte[buffer.LongLength];
+      buffer.CopyTo(output, 0L);
+      foreach (double sample in samples) {
+        int value = Convert.ToInt32(sample * int.MaxValue) >> ((4 - step) * 8);
+        try {
+          for (int k = 0; k < step; k++) {
+            output[i + (_little_endian ? k : step - 1 - k)] = unchecked((byte)value);
+            value >>= 8;
+          }
+        }
+        catch {
+          throw new ArgumentException("The buffer does not contain the original file or was corrupted.", "buffer");
+        }
+        i += step;
+      }
+      return output;
+    }
+
+    private int Read(byte[] buffer, long start, int length, bool? little_endian = null)
+    {
+      bool le = little_endian ?? _little_endian;
+      int value = 0;
+      if (length > 4 | length < 1)
+        throw new ArgumentException("Parameter must be in range [1,4].", "length");
+      if (le)
+        start += length - 1;
+      for (int i = 0; i < length; i++)
+        value = (value << 8) | buffer[start + (le ? -i : i)];
+      return value;
+    }
+  }
+}
